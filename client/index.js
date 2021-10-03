6d696b4e5fe18be2969e60c89d3c3d9ffde67ec5
Vasanth Anuraag Neppalli <neppallv@canisius.edu>
2021-10-03 17:02:36 -0400

    :zap:

--- client/index.js
+++ client/index.js
@@ -11,6 +11,7 @@ import App from "./components/App";
 import LoginForm from "./components/LoginForm";
 import SignUpForm from "./components/SignUpForm";
 import Dashboard from "./components/Dashboard";
+import requireAuth from "./components/requireAuth";
 
 const apolloClient = new ApolloClient({
   cache: new InMemoryCache(),
@@ -27,7 +28,7 @@ const Root = () => {
       <Route path="/" component={App}>
         <Route path="login" component={LoginForm} />
         <Route path="signup" component={SignUpForm} />
-        <Route path="dashboard" component={Dashboard} />
+        <Route path="dashboard" component={requireAuth(Dashboard)} />
       </Route>
     </Router>
   </ApolloProvider>
